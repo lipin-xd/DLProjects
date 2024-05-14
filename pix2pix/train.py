a1238a75e8d604fac3fa64c8f5f15cbe4d2e0924
李聪聪 <licongcong@LiccsMacBookPro.local>
2024-05-14 19:06:25 +0900

    调整参数

--- pix2pix/train.py
+++ pix2pix/train.py
@@ -28,7 +28,7 @@ def train_fn(disc, gen, loader, opt_disc, opt_gen, l1_loss, bce):
         D_fake_loss = bce(D_fake, torch.zeros_like(D_fake))
         D_loss = (D_real_loss + D_fake_loss) / 2
 
-        if i%5 == 0 :
+        if i % 5 == 0:
             disc.zero_grad()
             D_loss.backward()
             opt_disc.step()
@@ -36,8 +36,8 @@ def train_fn(disc, gen, loader, opt_disc, opt_gen, l1_loss, bce):
         # Train generator
         D_fake = disc(x, generated_img)
         G_fake_loss = bce(D_fake, torch.ones_like(D_fake))
-        L1 = l1_loss(generated_img, y)  # * config.L1_LAMBDA
-        G_loss = G_fake_loss + L1 / 100
+        L1 = l1_loss(generated_img, y) * config.L1_LAMBDA
+        G_loss = G_fake_loss + L1
 
         opt_gen.zero_grad()
         G_loss.backward()
@@ -61,7 +61,7 @@ def train():
     disc = Discriminator(in_channel=3).to(config.DEVICE)
     gen = Generator(in_channels=3).to(config.DEVICE)
     opt_disc = optim.Adam(disc.parameters(), lr=config.LEARNING_RATE, betas=(0.5, 0.999))
-    opt_gen = optim.Adam(gen.parameters(), lr=config.LEARNING_RATE*10, betas=(0.5, 0.999))
+    opt_gen = optim.Adam(gen.parameters(), lr=config.LEARNING_RATE * 10, betas=(0.5, 0.999))
     BCE = nn.BCEWithLogitsLoss()
     L1_LOSS = nn.L1Loss()
 
